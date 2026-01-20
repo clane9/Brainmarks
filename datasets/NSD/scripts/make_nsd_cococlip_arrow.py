bbc93bafff43ae658e8d81b778c058d001b428d9
Connor Lane <connor.lane858@gmail.com>
2026-01-20 16:29:46 +0000

    update nsd cococlip script

--- datasets/NSD/scripts/make_nsd_cococlip_arrow.py
+++ datasets/NSD/scripts/make_nsd_cococlip_arrow.py
@@ -1,12 +1,16 @@
 import argparse
 import logging
+import os
 import sys
 import tempfile
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 import datasets as hfds
 import numpy as np
 import pandas as pd
+from cloudpathlib import AnyPath, CloudPath, S3Client, S3Path
+from boto3.s3.transfer import TransferConfig
 
 import fmri_fm_eval.nisc as nisc
 import fmri_fm_eval.readers as readers
@@ -66,13 +70,20 @@ SUB_SES_SPLITS = {
 
 
 def main(args):
-    assert args.space in {"fslr64k", "schaefer400", "flat"}, f"{args.space} not yet supported"
+    valid_spaces = readers.VOLUME_SPACES | {"fslr64k", "schaefer400", "flat"}
+    assert args.space in valid_spaces, f"{args.space} not supported"
 
-    outdir = ROOT / f"data/processed/nsd-cococlip.{args.space}.arrow"
+    out_root = AnyPath(args.out_root or (ROOT / "data/processed"))
+    outdir = out_root / f"nsd-cococlip.{args.space}.arrow"
+
+    upload_client = get_s3_client()
+    if isinstance(outdir, CloudPath):
+        outdir = CloudPath(outdir, client=upload_client)
+
     _logger.info("Generating dataset: %s", outdir)
     if outdir.exists():
-        _logger.warning("Output %s exists; exiting.", outdir)
-        return 1
+        _logger.info("Output %s exists; exiting.", outdir)
+        return
 
     trial_df = pd.read_parquet(ROOT / "metadata/nsd_trial_metadata.parquet")
     include_trial_ids = np.load(ROOT / "metadata/nsd_include_trial_ids.npy")
@@ -98,6 +109,9 @@ def main(args):
     # all readers return a bold data array of shape (n_samples, dim).
     reader = readers.READER_DICT[args.space]()
     dim = readers.DATA_DIMS[args.space]
+
+    # root can be local or remote.
+    root = AnyPath(args.root or NSD_ROOT)
 
     # the bold data are scaled to mean 0, stdev 1 and then truncated to float16 to save
     # space. but we keep the mean and std to reverse this since some models need this.
@@ -131,10 +145,11 @@ def main(args):
                 features=features,
                 gen_kwargs={
                     "runs": runs,
-                    "root": NSD_ROOT,
+                    "root": root,
                     "trial_df": trial_df,
                     "targets": targets,
                     "reader": reader,
+                    "is_volume": args.space in readers.VOLUME_SPACES,
                 },
                 num_proc=args.num_proc,
                 split=hfds.NamedSplit(split),
@@ -144,8 +159,15 @@ def main(args):
             )
         dataset = hfds.DatasetDict(dataset_dict)
 
-        outdir.parent.mkdir(exist_ok=True, parents=True)
-        dataset.save_to_disk(outdir, max_shard_size="300MB")
+        if isinstance(outdir, S3Path):
+            _logger.info("Saving to s3: %s", outdir)
+            tmp_outdir = Path(tmpdir) / outdir.name
+            # in theory save_to_disk should support s3, but idk why it wasn't working
+            dataset.save_to_disk(tmp_outdir, max_shard_size="300MB")
+            outdir.upload_from(tmp_outdir)
+        else:
+            _logger.info("Saving locally: %s", outdir)
+            dataset.save_to_disk(outdir, max_shard_size="300MB")
 
 
 def generate_samples(
@@ -155,12 +177,9 @@ def generate_samples(
     trial_df: pd.DataFrame,
     targets: np.ndarray,
     reader: readers.Reader,
+    is_volume: bool = False,
 ):
-    for sub, ses, run in runs:
-        # nb, .lh path is passed but read_gifti_surf_data loads both hemispheres
-        path = f"{sub}/32k_fs_LR/timeseries/timeseries_session{ses:02d}_run{run:02d}.lh.func.gii"
-        fullpath = root / "nsddata_timeseries/ppdata" / path
-
+    for (sub, ses, run), path, fullpath in prefetch(root, runs, is_volume=is_volume):
         series = reader(fullpath)
         series, mean, std = nisc.scale(series)
 
@@ -192,8 +211,63 @@ def generate_samples(
             yield sample
 
 
+def prefetch(
+    root: AnyPath, runs: list[tuple[str, int, int]], is_volume: bool, *, max_workers: int = 1
+):
+    """Prefetch files from remote storage."""
+
+    with tempfile.TemporaryDirectory(prefix="prefetch-") as tmpdir:
+
+        def fn(run_tuple: tuple[str, int, int]):
+            sub, ses, run = run_tuple
+            if is_volume:
+                path = f"{sub}/MNI152/timeseries/timeseries_session{ses:02d}_run{run:02d}.nii.gz"
+            else:
+                # nb, .lh path is passed but read_gifti_surf_data loads both hemispheres
+                path = f"{sub}/32k_fs_LR/timeseries/timeseries_session{ses:02d}_run{run:02d}.lh.func.gii"
+
+            fullpath = root / "nsddata_timeseries/ppdata" / path
+            if isinstance(fullpath, CloudPath):
+                tmppath = Path(tmpdir) / path
+                tmppath.parent.mkdir(parents=True, exist_ok=True)
+                fullpath = fullpath.download_to(tmppath)
+            return run_tuple, path, fullpath
+
+        with ThreadPoolExecutor(max_workers) as executor:
+            futures = [executor.submit(fn, run_tuple) for run_tuple in runs]
+
+            for future in futures:
+                path, fullpath = future.result()
+                yield path, fullpath
+
+                if str(fullpath).startswith(tmpdir):
+                    fullpath.unlink()
+
+
+def get_s3_client():
+    config = TransferConfig(
+        multipart_threshold=8 * 1024 * 1024,
+        multipart_chunksize=8 * 1024 * 1024,
+        max_concurrency=10,
+        use_threads=True,
+    )
+
+    if "R2_ACCESS_KEY_ID" in os.environ:
+        client = S3Client(
+            aws_access_key_id=os.environ["R2_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["R2_SECRET_ACCESS_KEY"],
+            endpoint_url=os.environ["R2_ENDPOINT_URL_S3"],
+            boto3_transfer_config=config,
+        )
+    else:
+        client = S3Client(boto3_transfer_config=config)
+    return client
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--root", type=str, default=None)
+    parser.add_argument("--out-root", type=str, default=None)
     parser.add_argument(
         "--space", type=str, default="schaefer400", choices=list(readers.READER_DICT)
     )
