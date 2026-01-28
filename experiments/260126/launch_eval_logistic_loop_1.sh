18f9781924a651eb7d75fba5009489ae11d15644
Connor Lane <connor.lane858@gmail.com>
2026-01-28 12:02:13 +0000

    update scripts, notebooks, figures

---
+++ experiments/260126/launch_eval_logistic_loop_1.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=eval
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --time=infinite
+#SBATCH --partition=main
+#SBATCH --nodelist=n-2,n-3,n-4
+#SBATCH --output=slurms/slurm-%A_%a.out
+#SBATCH --account=training
+#SBATCH --array=0-6
+
+set -euo pipefail
+
+export OMP_NUM_THREADS=8
+
+ROOT="${HOME}/fmri-fm-eval"
+cd $ROOT
+
+# export all env variables
+set -a
+source .env.medarc.r2
+set +a
+
+EXP_NAME="260126"
+EXP_DIR="experiments/${EXP_NAME}"
+OUT_DIR="${EXP_DIR}/output"
+
+configs=(
+    connectome_schaefer400/cls
+    brainlm_vitmae_111m/patch
+    brain_jepa_vitb_ep300/patch
+    brain_harmonix_f/patch
+    brain_semantoks/cls
+    flat_mae_base_patch16_2/cls
+    swift/patch
+)
+
+datasets=(
+    adni_ad_vs_cn
+)
+
+# 7 models x 1 dataset
+
+num_datasets=${#datasets[@]}
+configid=$(($SLURM_ARRAY_TASK_ID / $num_datasets))
+datasetid=$(($SLURM_ARRAY_TASK_ID % $num_datasets))
+
+config=${configs[configid]}
+model=$(echo $config | cut -d / -f 1)
+repr=$(echo $config | cut -d / -f 2)
+
+dataset=${datasets[datasetid]}
+
+base_config="${EXP_DIR}/logistic_loop.yaml"
+overrides=""
+
+name="eval_logistic_loop/${dataset}__${model}__${repr}"
+result="${OUT_DIR}/${name}/eval_table.csv"
+if [[ -f $result ]]; then
+    echo "result $result exists; skipping"
+    exit
+fi
+
+notes="logistic loop eval sweep ${EXP_NAME} (${dataset} ${model} ${repr})"
+
+uv run --no-sync python -W ignore -m fmri_fm_eval.main_logistic_loop \
+    $model \
+    $repr \
+    $dataset \
+    --config \
+    $base_config \
+    --overrides \
+    output_root="${OUT_DIR}" \
+    name="${name}" \
+    notes="${notes}" \
+    $overrides
